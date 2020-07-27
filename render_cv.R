3127d6130e9bc1a32a2e2c42d67a0e464298e75c
DFJL <felipe_jimenez_l@yahoo.com>
2020-07-27 23:38:28 +0000

    Changes in render.r file

--- render_cv.R
+++ render_cv.R
@@ -12,7 +12,7 @@ source("CV_printing_functions.r")
  cv_data <- create_CV_object(
    data_location = "https://docs.google.com/spreadsheets/d/1Vo3hjj47wuq1I8PDmw2Gq0UpKtvqkJJtcLTS8hlJGxw/edit?usp=sharing",
    cache_data = TRUE)
-cache_data <- TRUE
+cache_data <- FALSE
 
 # Knit the HTML version
 rmarkdown::render("dfjlCv.Rmd",
@@ -29,3 +29,4 @@ rmarkdown::render("dfjlCv.Rmd",
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "dfjl_cv.pdf")
+
